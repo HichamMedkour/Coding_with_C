ee93501e92da909b28e12b594649016f05a4bb0f
Hicham Medkour <84011751+HichamMedkour@users.noreply.github.com>
2023-09-04 23:26:58 +0100

    Add files via upload
    The code shows how powerful macros are. One cautionary tale, nowadays in the C and C++ community, there's much less reliance on macros especially ones that involve code. In general for code segments, we either let the compiler optimize it or for example in the C++ language and some C compilers you can call a function inline and that asks the compiler to optimize it much like a would be for macro and that is much safer code, more check

---
+++ Working with Preprocessor code and macros.c
@@ -0,0 +1,116 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//enum type {sun,mon,tues,wedn,thurs,frid,sat}; // define new type
+//
+//typedef enum type xxx;
+//void print_day(xxx d)
+//{
+//        switch (d){
+//                case sun:printf("sunday");break;
+//                case mon:printf("monday");break;
+//                case tues:printf("tuesday");break;
+//                case wedn:printf("wednesday");break;
+//                case thurs:printf("thursday");break;
+//                case frid:printf("friday");break;
+//                case sat:printf("saturday");break;
+//                default:printf("error %d\n",d);
+//            }
+//
+//}
+//
+//xxx next_day (xxx d)
+//{
+//    return (d + 1);
+//}
+//xxx yesterday(xxx d)
+//{
+//    return (d-1);
+//};
+//
+//
+//int main()
+//{
+//   xxx today = sun;
+//   print_day(today);
+//  //printf("\n\n");
+//  //print_day(7);
+//   printf("\n\n");
+//   print_day(next_day(today));
+//   printf("\n\n");
+//   print_day(yesterday(today));
+//   printf("\n\n");
+//
+//    return 0;
+//}
+//////////////////////////////////////////////////////////////
+
+/*
+ This code show some of the use of the preprocessor
+ Date: 04/09/2023
+ Author: Hicham Medkour
+ This peace of code is going to generate a random a thousand
+ of southern male elephant seal weights.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* The aim is to produce a macro that randomly generates a male weight,
+   so we use very simple macro here: the minimum and the maximum.
+   Note: female weight range is 880-2000 lbs(Pound) (1 lbs=0.453592 kg)*/
+#define MAX_ELEPHANT_SEAL_WT_MALE 8800
+#define MIN_ELEPHANT_SEAL_WT_MALE 4400
+
+/* The gape between min and max */
+#define RANGE         4400
+
+/* We want to generate a population 1000 sample weights */
+#define POPULATION    1000
+
+/* Because we want something within the range 4400-8800 */
+#define WEIGHT_OVER   rand()% RANGE
+
+/* If WEIGHT_OVER is less than 4400, the actual WEIGHT will be within the range 4400-8800 */
+#define WEIGHT        WEIGHT_OVER + MIN_ELEPHANT_SEAL_WT_MALE
+
+/* Here we write multi-line macro to generate the weight samples
+and fill the table with them */
+#define FILL          for(i=0; i < POPULATION; i++)\
+                           data[i]= WEIGHT
+
+/* This is a basic function to print out the data */
+void print_data( int d[], int size)
+{
+   int i;
+   for (i=0; i<size; i++)
+   {
+       printf("%d\t",d[i]); //keep printing on the same line
+       if ((i+1)%10 == 0)  printf("\n"); // go to new line after 10 values printed on the same line
+   }
+}
+
+int main(void)
+{
+    int i;
+
+    /* None here the advantage of macro:
+     we can easily redefine the value of population above
+     for exemple: we reuse this code to check for Northern elephant seals */
+    int data[POPULATION];
+
+    /*Here when we use the random number generator we want
+    each time to call the program to have something different,
+    and this "srand(time(0))" allows us to reset the seed based on the system's clock
+    and we don't always get the same value*/
+    srand(time(0));
+
+    /*Then we call FILL.
+    Notice: FILL and all the stuff depends on preprocessor*/
+    FILL;
+
+    print_data(data,POPULATION);
+    printf("\n\n");
+
+    return 0;
+}
